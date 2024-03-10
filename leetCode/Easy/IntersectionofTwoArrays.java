b66e473e18274d7c05612ca8fe5bf9b17ca49406
Turpal Baysarov <turpal.baysarov@bk.ru>
2024-03-10 12:55:01 +0300

    Commit all task

---
+++ leetCode/Easy/IntersectionofTwoArrays.java
@@ -0,0 +1,29 @@
+package leetCode.Easy;
+
+import java.util.*;
+
+public class IntersectionofTwoArrays {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> setList = new HashSet<>();
+        Set<Integer> setListUnique = new HashSet<>();
+        for (int num : nums1){
+            setList.add(num);
+        }
+        for (int num : nums2){
+            if (setList.contains(num)){
+                setListUnique.add(num);
+            }
+        }
+        return setListUnique
+                .stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+    }
+
+    public static void main(String[] args) {
+        for (int i : intersection(new int []{9,4,9,8,4}, new int []{9,4,9,4})){
+            System.out.println(i);
+        }
+    }
+}
