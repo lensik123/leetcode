0ec77d6ce18b21d2ef5f4ae973cd73daf06310cc
Turpal Baysarov <turpal.baysarov@bk.ru>
2024-03-14 16:20:35 +0300

    Commit all task

---
+++ leetCode/Medium/BinarySubarraysWithSum.java
@@ -0,0 +1,56 @@
+package leetCode.Medium;
+
+//Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+//
+//A subarray is a contiguous part of the array.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,0,1,0,1], goal = 2
+//Output: 4
+//Explanation: The 4 subarrays are bolded and underlined below:
+//[1,0,1,0,1]
+//[1,0,1,0,1]
+//[1,0,1,0,1]
+//[1,0,1,0,1]
+//Example 2:
+//
+//Input: nums = [0,0,0,0,0], goal = 0
+//Output: 15
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinarySubarraysWithSum {
+
+    public static void main(String[] args) {
+
+        numSubarraysWithSum(new int[]{1, 0, 1, 0, 1}, 2);
+
+    }
+
+    public static int numSubarraysWithSum(int[] nums, int goal) {
+        int totalCount = 0;
+        int currentSum = 0;
+        // {prefix: number of occurrence}
+        Map<Integer, Integer> freq = new HashMap<>(); // To store the frequency of prefix sums
+
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum == goal){
+                totalCount++;
+            }
+
+            // Check if there is any prefix sum that can be subtracted from the current sum to get the desired goal
+            if (freq.containsKey(currentSum - goal)){
+                totalCount += freq.get(currentSum - goal);
+            }
+
+            freq.put(currentSum, freq.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return totalCount;
+    }
+}
